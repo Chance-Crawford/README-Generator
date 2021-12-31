a973977dabb85cee2a5c38f5182ac0450724dd70
Chance <chancecrawford30@gmail.com>
2021-12-31 11:15:21 -0800

    license for read me generates based on user selection

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,40 @@
 // a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(answerObj) {
+
+  // if there is no license added by user,
+  // return empty string
+  if(!answerObj.hasOwnProperty("license")){
+    return ``;
+  }
+
+  let license = answerObj.license[0];
+
+  if(license === "MIT"){
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  }
+  else if(license === "GPL 3.0"){
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  }
+  else if(license === "Apache"){
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  }
+  else if(license === "GPL 2.0"){
+    return `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+  }
+  else if(license === "BSD"){
+    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  }
+  else{
+    return ``;
+  }
+}
 
 // a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
+
+
 
 // a function that returns the sections based
 // on whether the user added them or not, if not returns empty string
@@ -73,6 +103,8 @@ function renderSections(answerObj) {
   return sectionString;
 }
 
+
+
 // a function to generate markdown for README
 function generateMarkdown(answerObj) {
 
@@ -80,6 +112,7 @@ function generateMarkdown(answerObj) {
 
   return `
   # ${answerObj.title}
+  ${renderLicenseBadge(answerObj)}
 
   ## Description
   ${answerObj.description}
